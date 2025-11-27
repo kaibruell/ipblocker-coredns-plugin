cc7c0e99b526b3057ec45494ae1011ff2b35c803
kaibruell <kaibruell@gmx.de>
2025-11-27 16:42:16 +0100

    Testing printing to docker console...

--- setup.go
+++ setup.go
@@ -1,12 +1,19 @@
 package ipblocker
 
 import (
+	"fmt"
+
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 )
 
-func init() { plugin.Register("ipblocker", setup) }
+func init() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Hello CoreDNS Container from ipblocker plugin!")
+	}
+	plugin.Register("ipblocker", setup)
+}
 
 func setup(c *caddy.Controller) error {
 	if c.Next() {
